8a19e0bf5fc8461af1965f678bdff4330a7e6b72
julry <julry6999@gmail.com>
2020-09-03 19:07:21 +0300

    redesign intro and final

--- src/hocs/useResult.js
+++ src/hocs/useResult.js
@@ -3,7 +3,7 @@ import { ProgressContext } from '../contexts/ProgressContext';
 import { getAnswerById } from '../utils/getAnswerById';
 import { AnswerType, answerTypes } from '../answerTypes.config';
 
-const DEFAULT_RESULT = AnswerType.Sales;
+const DEFAULT_RESULT = AnswerType.ProjectOffice;
 
 export const useResult = () => {
     const { answers } = useContext(ProgressContext);
