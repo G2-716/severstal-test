b412535387fca71af3fec237ff30344990e4d4c2
Дмитрий Маньшин <manshin.dmitry@gmail.com>
2020-09-06 02:39:10 +0300

    Remove unused code

--- src/components/Button/sharing/FacebookShareButton.js
+++ src/components/Button/sharing/FacebookShareButton.js
@@ -1,29 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { ShareButton } from './ShareButton';
 import { FbIcon } from '../../svg/SocialMediaIcons/FbIcon';
-import { createOpenGraphMeta } from '../../../utils/createOpenGraphMeta';
 
 export const FacebookShareButton = ({ shareParams }) => {
-    const { url, title, description, image } = shareParams;
+    const { url } = shareParams;
     const queryParams = new URLSearchParams();
     queryParams.append('u', url);
-
-    const ogMetas = [
-        ['title', title],
-        ['description', description],
-        ['type', 'website'],
-        ['url', url],
-        ['image', image],
-    ];
-
-    useEffect(() => {
-        const head = document.head;
-        const fragment = document.createDocumentFragment();
-        const metaElements = ogMetas.map(meta => fragment.appendChild(createOpenGraphMeta(...meta)));
-        head.appendChild(fragment);
-
-        return () => metaElements.forEach(meta => head.removeChild(meta));
-    });
 
     return (
         <ShareButton href={`https://www.facebook.com/sharer/sharer.php?${queryParams.toString()}`}>
