c492727ba6d5da1c7d4f71d5e4abe0125930d0ed
Дмитрий Маньшин <manshin.dmitry@gmail.com>
2020-09-09 12:03:09 +0300

    Add build

---
+++ build/precache-manifest.20c09c490eb9ae8ae7fbb8099b348631.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7312788b68495bddefdc95e961283a03",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d60fb232058f08a21719",
+    "url": "./static/js/2.1b979c5b.chunk.js"
+  },
+  {
+    "revision": "863004d46cd7375f6c043c934b2ed4e3",
+    "url": "./static/js/2.1b979c5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d535a015b08be12b935",
+    "url": "./static/js/main.99c388e9.chunk.js"
+  },
+  {
+    "revision": "693cf95d6194cb3e49b6",
+    "url": "./static/js/runtime-main.66f4a047.js"
+  }
+]);
